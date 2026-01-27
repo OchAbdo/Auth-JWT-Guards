2a53fe1d5c35ff3214135433a42b5487145467e3
OchAbdo <ochabdo02@gmail.com>
2026-01-27 09:33:21 +0100

    intall package of jwt and passport . Set up the jwt module and passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,24 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])],
+  imports:[TypeOrmModule.forFeature([User]),
+      PassportModule.register({defaultStrategy : 'jwt'}),
+      JwtModule.registerAsync({
+        imports:[ConfigModule],
+        inject : [ConfigService],
+        useFactory : (configservice : ConfigService)=>({
+          secret: configservice.get<string>("SECRECT_JWT"),
+          signOptions:{
+            expiresIn : configservice.get<number>("EXPIRED_TIME")
+          }
+        })
+      })
+],
   controllers: [AuthController],
   providers: [AuthService , UserRepository],
 })
